de7094641436251215e4999916b76ef2cd06ee17
alexsandra.souto <alexsandra.souto@virtus.ufcg.edu.br>
2024-09-10 16:10:47 -0300

    Versão final

--- main.c
+++ main.c
@@ -9,28 +9,40 @@ typedef struct {
     int period;
     int execution_time;
     double utilization;
+    int priority;
 } Task;
 
-void swap(Task *xp, Task *yp) {
-    // Função que troca a posição de duas tarefas
-    Task temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void ordenar_tarefas(Task tasks[], int n) {
-    // Esta função realiza a ordenação das tarefas
-    // Tarefas com períodos menores recebem maior prioridade
+void order_tasks(Task tasks[], int n) {
+    // Esta função realiza a ordenação das tarefas, periodos menores primeiro
     int i, j;
+    Task aux;
     for (i = 0; i < n-1; i++) {
         for (j = 0; j < n-i-1; j++) {
-            if (tasks[j].period > tasks[j+1].period)
-                swap(&tasks[j], &tasks[j+1]);
+            if (tasks[j].period > tasks[j+1].period){
+                aux = tasks[j];
+                tasks[j] = tasks[j+1];
+                tasks[j+1] = aux;
+            }
         }
     }
 }
 
-double calcula_utilizacao(Task tasks[], int n) {
+
+void set_priority(Task tasks[], int n){
+    // Esta função atribui a prioridade das tarefas ordenadas por periodo de forma crescente
+    // Tarefas com períodos menores recebem maior prioridade
+    int i;
+    tasks[0].priority = 1;
+    for (i = 1; i < n; i++){
+        if (tasks[i].period == tasks[i-1].period)
+            tasks[i].priority = tasks[i-1].priority;
+        else 
+            tasks[i].priority = i + 1;
+    }
+
+}
+
+double calculate_utilization(Task tasks[], int n) {
     // Esta função recebe uma lista de struct Task e calcula a 
     // taxa total de utilização
     double total_utilization = 0;
@@ -45,16 +57,16 @@ int main() {
     // ------------------------------- LEITURA E ANÁLISE DO ARQUIVO JSON --------------------------------------------
 
     FILE *fp; // Apontador para arquivo, para manipular o arquivo JSON
-    struct json_object *parsed_json; // Apontador para o objeto JSON
-    struct json_object *tasks_array; // Apontador para lista de tarefas
-    struct json_object *task_obj; // Apontador para uma tarefa especifica
-    struct json_object *id, *period, *execution_time; // Apontadores para as informações das tarefas
+    struct json_object *parsed_json; 
+    struct json_object *tasks_array; 
+    struct json_object *task_obj; 
+    struct json_object *id, *period, *execution_time;
     
     size_t n_tasks;
     size_t i;
 
     // Abertura do arquivo json
-    fp = fopen("tasks.json", "r");
+    fp = fopen("tasks2.json", "r");
     if (fp == NULL) {
         fprintf(stderr, "Não foi possível abrir o arquivo.\n");
         return 1;
@@ -72,11 +84,12 @@ int main() {
 
     // --------------------- PREENCHIMENTO E LEITURA DA STRUCT TASK ----------------------------------
 
-    Task tasks[n_tasks];  // Cria o array de structs referente às tarefas 
+    // Cria o array de structs referente as tarefas 
+    Task tasks[n_tasks];
 
     // Loop para percorrer as tarefas para extrar as informações 
     for (i = 0; i < n_tasks; i++) {
-        task_obj = json_object_array_get_idx(tasks_array, i);  // recebe a task atual
+        task_obj = json_object_array_get_idx(tasks_array, i);
 
         json_object_object_get_ex(task_obj, "id", &id);
         json_object_object_get_ex(task_obj, "period", &period);
@@ -89,16 +102,17 @@ int main() {
 
     // ------------------------------ CÁLCULO DE UTILIZAÇÃO -------------------------------------
 
-    double utilization = calcula_utilizacao(tasks, n_tasks);           // Cálculo da utilização pelas tarefas
+    double utilization = calculate_utilization(tasks, n_tasks);           // Cálculo da utilização pelas tarefas
     double utilization_limit = n_tasks * (pow(2, 1.0/n_tasks) - 1);   // Cálculo do limite de utilização
 
-    ordenar_tarefas(tasks, n_tasks); // Orndeação das tarefas
+    order_tasks(tasks, n_tasks); // Orndeação das tarefas
+    set_priority(tasks, n_tasks); // Atribui a priporidade das tarefas
 
     struct json_object *output_json = json_object_new_object();  // Criação do objeto JSON de saída
 
     // Verifica se a utilização é menor ou igual ao limite e retorna a viabilidade em string
     struct json_object *schedulability = json_object_new_string(utilization <= utilization_limit ? "viable" : "not viable"); 
-    json_object_object_add(output_json, "schedulability", schedulability);  // Adiciona a informação de viabilidade ao objeto JSON de saída
+    json_object_object_add(output_json, "schedulability", schedulability);  
 
     // ------------------------------------ ORDENAÇÃO -------------------------------------------
 
@@ -107,10 +121,10 @@ int main() {
 
     struct json_object *suggested_schedule = json_object_new_array();
     for (i = 0; i < n_tasks; i++) {
-        struct json_object *task_json = json_object_new_object(); // Um objeto JSON vazio é criado para armazenar as informações da tarefa atual
+        struct json_object *task_json = json_object_new_object();
         json_object_object_add(task_json, "id", json_object_new_string(tasks[i].id));
-        json_object_object_add(task_json, "priority", json_object_new_int(i + 1));
-        json_object_array_add(suggested_schedule, task_json); // Adiciona à tarefa ao JSON de sugestão na ordem que está no arrayy
+        json_object_object_add(task_json, "priority", json_object_new_int(tasks[i].priority));
+        json_object_array_add(suggested_schedule, task_json); 
     }
     json_object_object_add(output_json, "suggested_schedule", suggested_schedule); // Adiciona a sugestão de escalonamento ao JSON de saída
 
@@ -126,7 +140,8 @@ int main() {
     }
 
     // Escrever o JSON no arquivo
-    fprintf(fp_saida, "%s", output_json);
+    const char *json_str = json_object_to_json_string(output_json);
+    fprintf(fp_saida, "%s", json_str);
     fclose(fp_saida);
 
     json_object_put(parsed_json); 
@@ -134,3 +149,4 @@ int main() {
 
     return 0;
 }
+
